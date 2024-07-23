3330e2aecc367e9fc68a2b679ebddcb2a9c49cb5
codewithsw0rd <thugkaustuv@gmail.com>
2024-07-23 18:19:10 +0545

    Hangman art

--- hangman.py
+++ hangman.py
@@ -1,2 +1,25 @@
 # Hangman
 import random
+
+words =("apple", "ball", "cat", "dog", "elephant")
+
+# dictionary of key
+hangman_art = {0: ("   ",
+                   "   ",
+                   "   "),
+               1: (" O  ",
+                   "   ",
+                   "   "),
+               2: (" O  ",
+                   " |  ",
+                   "   "),
+               3: ("   ",
+                   "   ",
+                   "   "),
+               4: ("   ",
+                   "   ",
+                   "   "),
+               5: ("   ",
+                   "   ",
+                   "   "),
+               6: ()}
