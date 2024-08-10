494cea69ea8ae3e3af8c82aaa701464e35515ca5
codewithsw0rd <thugkaustuv@gmail.com>
2024-08-10 08:56:26 +0545

    Simple Hangman Game completed.

---
+++ wordslist.py
@@ -0,0 +1,31 @@
+# Set of words for Hangman game
+
+words = [
+    # Animals
+    'giraffe', 'kangaroo', 'dolphin', 'octopus', 'penguin',
+    'squirrel', 'crocodile', 'chameleon', 'flamingo', 'rhinoceros',
+
+    # Fruits
+    'pineapple', 'strawberry', 'blueberry', 'watermelon', 'grapefruit',
+    'pomegranate', 'raspberry', 'apricot', 'blackberry', 'nectarine',
+
+    # Countries
+    'Australia', 'Germany', 'Argentina', 'Portugal', 'Malaysia',
+    'Venezuela', 'Switzerland', 'Indonesia', 'Zimbabwe', 'Kazakhstan',
+
+    # Professions
+    'architect', 'scientist', 'astronaut', 'musician', 'librarian',
+    'journalist', 'pharmacist', 'geologist', 'engineer', 'surgeon',
+
+    # Objects
+    'telescope', 'refrigerator', 'binoculars', 'skateboard', 'microphone',
+    'typewriter', 'compass', 'harmonica', 'headphones', 'calculator',
+
+    # Colors
+    'turquoise', 'lavender', 'maroon', 'fuchsia', 'chartreuse',
+    'vermilion', 'aquamarine', 'cerulean', 'magenta', 'indigo',
+
+    # Foods
+    'lasagna', 'croissant', 'tortilla', 'dumplings', 'macaroni',
+    'cheesecake', 'pretzel', 'schnitzel', 'falafel', 'gnocchi'
+]
